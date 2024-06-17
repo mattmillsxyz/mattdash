170c1b74cdedb33a62aff310493c07298e830293
Matt Mills <mattmillsxyz@gmail.com>
2024-06-16 19:27:52 -0500

    Display email from context

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,37 +1,17 @@
 // app/profile/page.tsx
 'use client';
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
-import { db } from '../../firebaseConfig';
-import { doc, getDoc } from 'firebase/firestore';
-
-interface UserData {
-  email: string;
-}
 
 const Profile = () => {
   const { user } = useContext(UserContext);
-  const [userData, setUserData] = useState<UserData | null>(null);
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      if (user) {
-        const userDoc = await getDoc(doc(db, 'users', user.uid));
-        if (userDoc.exists()) {
-          setUserData(userDoc.data() as UserData);
-        }
-      }
-    };
-
-    fetchUserData();
-  }, [user]);
 
   return (
     <div>
       <h1>Profile</h1>
-      {userData && (
+      {user && (
         <div>
-          <p>Email: {userData.email}</p>
+          <p>Email: {user.email}</p>
         </div>
       )}
     </div>
